4aed8fc26004c56ec2234a422d0960eed395fa67
Luke Fernandez <luke.matthew.fernandez@gmail.com>
2023-06-09 13:56:34 -0700

    implement create course trasnaction

--- models/course.js
+++ models/course.js
@@ -5,10 +5,23 @@ const { extractValidFields } = require('../lib/validation');
 const DB_COLLECTION_NAME = 'courses';
 
 const CourseSchema = {
-  _id: { required: true, unique: true },
   subject: { required: true },
   number: { required: true },
   title: { required: true },
   term: { required: true },
   instructorId: { required: true }
+}
+
+/**
+ * Create a new course in the database
+ * @param course - the course schema to create
+ * @returns {Promise<unknown>} - the id of the created course
+ */
+async function createCourse(course) {
+  return new Promise((resolve) => {
+    course = extractValidFields(course, CourseSchema);
+    getDbReference().collection(DB_COLLECTION_NAME).insertOne(course).then(result => {
+      resolve(result.insertedId);
+    });
+  });
 }
