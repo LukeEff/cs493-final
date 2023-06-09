c1fad5529fbb7f6c2ec68783f489cba0e59f43ab
Luke Fernandez <luke.matthew.fernandez@gmail.com>
2023-06-09 12:29:51 -0700

    Add create user model function

--- models/user.js
+++ models/user.js
@@ -1,4 +1,9 @@
 const { ObjectId } = require('mongodb');
+const { getDbReference } = require('../lib/mongo');
+const { extractValidFields } = require('../lib/validation');
+const { jsonwebtoken } = require('jsonwebtoken');
+
+const DB_COLLECTION_NAME = 'users';
 
 const ROLES = {
   ADMIN: 'admin',
@@ -8,8 +13,24 @@ const ROLES = {
 
 const UserSchema = {
   name: { required: true },
-  email: { required: true },
+  email: { required: true, unique: true },
   password: { required: true },
+  role: { required: true }
+}
+
+/**
+ * Creates a new user in the database
+ * @param user - the user schema to create
+ * @returns {Promise<unknown>} - the id of the created user
+ */
+async function createUser(user) {
+  return new Promise((resolve, reject) => {
+    user = extractValidFields(user, UserSchema);
+    user.role ??= ROLES.USER;
+    getDbReference().collection(DB_COLLECTION_NAME).insertOne(user).then(result => {
+      resolve(result.insertedId);
+    });
+  });
 }
 
 exports.ROLES = ROLES;
