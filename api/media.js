462bfe37f0981c967b776e8f68e14b7e1d0b7688
Luke Fernandez <luke.matthew.fernandez@gmail.com>
2023-06-13 19:00:05 -0700

    require auth for downloading files

--- api/media.js
+++ api/media.js
@@ -1,11 +1,14 @@
 const { Router } = require('express');
 
+const { reqAuthentication } = require('../lib/auth');
+
+
 const { downloadSubmissionById, containsSubmission } = require("../models/assignment");
 
 const router = Router();
 
 // Should we allow anybody to download any submission?
-router.get('/submissions/:id', async (req, res, next) => {
+router.get('/submissions/:id', reqAuthentication, async (req, res, next) => {
   try {
     if (!await containsSubmission(req.params.id)) {
       res.status(404).send({
