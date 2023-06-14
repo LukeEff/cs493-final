c2b6cc8023a21e3216a8ab630314edeede684138
Luke Fernandez <luke.matthew.fernandez@gmail.com>
2023-06-13 18:58:44 -0700

    create endpoint for downloading submission file

---
+++ api/media.js
@@ -0,0 +1,25 @@
+const { Router } = require('express');
+
+const { downloadSubmissionById, containsSubmission } = require("../models/assignment");
+
+const router = Router();
+
+// Should we allow anybody to download any submission?
+router.get('/submissions/:id', async (req, res, next) => {
+  try {
+    if (!await containsSubmission(req.params.id)) {
+      res.status(404).send({
+        error: "Submission not found."
+      })
+      return
+    }
+    await downloadSubmissionById(req.params.id, res)
+  } catch (err) {
+    console.error(err)
+    res.status(500).send({
+      error: "Unable to fetch submission."
+    })
+  }
+});
+
+module.exports = router;
